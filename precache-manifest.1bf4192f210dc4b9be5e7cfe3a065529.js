49834c19ad1059568226fcea1e83ce5e9b41aea9
Nobel <nobelinderpreet91@gmail.com>
2020-10-09 19:40:15 +1100

    Updates

---
+++ precache-manifest.1bf4192f210dc4b9be5e7cfe3a065529.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e50cf38a24f7893227c01416b35b4c68",
+    "url": "/MUIReactTodoApp/index.html"
+  },
+  {
+    "revision": "3bb34b8842865a3bd778",
+    "url": "/MUIReactTodoApp/static/css/main.213b67d5.chunk.css"
+  },
+  {
+    "revision": "a6500323a04acab92867",
+    "url": "/MUIReactTodoApp/static/js/2.b8c544a0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/MUIReactTodoApp/static/js/2.b8c544a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bb34b8842865a3bd778",
+    "url": "/MUIReactTodoApp/static/js/main.5126e578.chunk.js"
+  },
+  {
+    "revision": "2a5ee520a8421070fbe5",
+    "url": "/MUIReactTodoApp/static/js/runtime-main.082f8447.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/MUIReactTodoApp/precache-manifest.1bf4192f210dc4b9be5e7cfe3a065529.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/MUIReactTodoApp/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
